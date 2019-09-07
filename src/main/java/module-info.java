96be2f416786b38f7006d66695aa2d3797ac56b9
dennisbejze <bejdenn@hotmail.de>
2019-09-07 08:42:31 +0200

    Updated project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module hellofx {
+	exports customElements;
+	exports projectTeacher;
+
+	requires java.desktop;
+	requires javafx.base;
+	requires javafx.controls;
+	requires transitive javafx.graphics;
+	requires SQLite;
+
+}
